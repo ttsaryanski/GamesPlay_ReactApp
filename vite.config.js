5d20e0e5ec3af4700262a77456189fad699ebcca
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-06-23 17:32:10 +0300

    Add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "GamesPlay App",
+                short_name: "GamesPlay",
+                start_url: "/",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#2c3e50",
+            },
+        }),
+    ],
     base: "/",
-    // server: {
-    //     port: 3333,
-    // },
+    server: {
+        historyApiFallback: true,
+        //port: 3333,
+    },
 });
